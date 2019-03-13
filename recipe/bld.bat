42d3ebc807c576d2fa36549803a499ae5be19833
A. Stamminger <andreas.stamminger@pm.me>
2019-03-13 23:50:47 +0100

    Correct path delimiters in windows build script

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,4 @@
-SET PATH_TO_FILE=%SRC_DIR%/tests/staticfiles_tests/apps/test/static/test/⊗.txt
+SET PATH_TO_FILE=%SRC_DIR%\tests\staticfiles_tests\apps\test\static\test\⊗.txt
 del %PATH_TO_FILE%
 
 "%PYTHON%" -m pip install --no-deps --ignore-installed .
