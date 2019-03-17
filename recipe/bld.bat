81e16c3b9eacbd66892fb8fd34cdccb44527db54
Andreas Stamminger <astamminger@users.noreply.github.com>
2019-03-17 20:06:30 +0100

    Set correct file name in Windows build script
    Bytes of UTF-8 character in the file name are treated as individual bytes rather than as a sequence.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,4 @@
-SET PATH_TO_FILE=%SRC_DIR%\tests\staticfiles_tests\apps\test\static\test\⊗.txt
-del %PATH_TO_FILE%
+SET STATIC_TEST_FOLDER=%SRC_DIR%\tests\staticfiles_tests\apps\test\static\test\
+powershell -Command "rm $('%STATIC_TEST_FOLDER%'+[Char]0xE2+[Char]0x8A+[Char]0x97+'.txt')"
 
 "%PYTHON%" -m pip install --no-deps --ignore-installed .
